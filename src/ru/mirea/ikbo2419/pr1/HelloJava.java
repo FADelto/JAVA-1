46cffcbdaa07aabb80c2ad3040e79fd7b21bf7e4
FADelto <64438093+FADelto@users.noreply.github.com>
2020-09-02 10:13:49 +0300

    Initial commit

---
+++ src/ru/mirea/ikbo2419/pr1/HelloJava.java
@@ -0,0 +1,8 @@
+package ru.mirea.ikbo2419.pr1;
+import java.lang.System;
+import java.util.*;
+public class HelloJava{
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+}
+}
